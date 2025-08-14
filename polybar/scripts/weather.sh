f40757f27a4cee3ec016f9b5ee8635e8020676b5
Alexander <a.vikt5@mail.ru>
2025-08-14 13:59:58 +0300

    Update weather.sh

--- polybar/scripts/weather.sh
+++ polybar/scripts/weather.sh
@@ -8,4 +8,7 @@ data_path='.weather[].main,.main.temp,.main.humidity,.wind.speed'
 data=$(curl -sf "https://api.openweathermap.org/data/2.5/weather?lat=$LAT&lon=$LON&units=metric&lang=en&appid=$OPENWEATHERMAP_API_KEY" | jq -r $data_path)
 read MAIN TEMP HUM WSPEED <<< $(echo $data)
 DELIM="%{F#ffcc99}|%{F-}"
-echo " $MAIN_ICON $MAIN $DELIM$TEMP󰔄$DELIM  "$WSPEED"m/s"
+[ "$MAIN" == "Clear" ] && MAIN_ICON="󰖙"
+[ "$MAIN" == "Clouds" ] && MAIN_ICON="󰖐"
+[ "$MAIN" == "Rain" ] && MAIN_ICON="󰖗"
+echo "$MAIN_ICON $MAIN$DELIM$TEMP󰔄$DELIM "$WSPEED"m/s"
